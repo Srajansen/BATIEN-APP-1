6306918621fbd1751051f27f584978c12c2dee4b
Srajan sen <114805738+Srajansen@users.noreply.github.com>
2023-01-21 17:17:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navs from './Navs';
+import './App.css';
+import {UserProvider} from './context/userContext'
+import {DarkProvider} from './context/DarkmodeContext'
+function App() {
+  return (
+    <div className="App">
+      <UserProvider>
+        <DarkProvider>
+      <Navs/>
+      </DarkProvider>
+      </UserProvider>
+    </div>
+  );
+}
+
+export default App;
